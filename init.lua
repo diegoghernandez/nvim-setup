5ff2f19d48d782106ede5aaa094e296021109b57
diegoghernandez <diegoj4v@gmail.com>
2025-03-26 18:44:51 -0600

    Update plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
 require("pnvmc.core")
 require("pnvmc.lazy")
 
+vim.opt.background = "dark" -- set this to dark or light
+vim.cmd("colorscheme oxocarbon")
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+   callback = function()
+      vim.lsp.buf.format {
+         async = false,
+      }
+   end,
+})
---
+++ lua/pnvmc/config/treesitter/parsers.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+function M.install_automatically()
+   return {
+      'astro',
+      'css',
+      'diff',
+      'dockerfile',
+      'git_config',
+      'git_rebase',
+      'gitattributes',
+      'gitcommit',
+      'gitignore',
+      'html',
+      'javascript',
+      'jsdoc',
+      'json',
+      'jsonc',
+      'lua',
+      'luadoc',
+      'luap',
+      'markdown',
+      'markdown_inline',
+      'python',
+      'regex',
+      'requirements',
+      'scheme',
+      'tsx',
+      'typescript',
+      'vim',
+      'vimdoc',
+      'vue',
+      'xml',
+      'yaml',
+   }
+end
+
+return M
