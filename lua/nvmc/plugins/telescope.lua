dcdded121f708392337ac30b4a59d6a0a9cc16d5
diegoghernandez <diegoj4v@gmail.com>
2025-05-29 12:13:54 -0600

    Install package-info plugin

--- lua/nvmc/plugins/telescope.lua
+++ lua/nvmc/plugins/telescope.lua
@@ -57,12 +57,14 @@ return { -- Fuzzy Finder (files, lsp, etc)
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
         },
+        package_info = {},
       },
     }
 
     -- Enable Telescope extensions if they are installed
     pcall(require('telescope').load_extension, 'fzf')
     pcall(require('telescope').load_extension, 'ui-select')
+    pcall(require('telescope').load_extension, 'package_info')
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
