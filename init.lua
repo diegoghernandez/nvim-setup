96890fee7c824059b529d1a2ce454dc087b15ab2
diegoghernandez <diegoJ4V@gmail.com>
2025-06-23 14:53:32 -0600

    Change diagnostic config to use virtual lines instead of text

--- init.lua
+++ init.lua
@@ -3,6 +3,11 @@ require 'nvmc.core.keymaps'
 require 'nvmc.core.autocommands'
 require 'nvmc.core.lazy'
 
+vim.diagnostic.config {
+  virtual_text = false,
+  virtual_lines = true,
+}
+
 vim.cmd.colorscheme 'catppuccin'
 
 vim.filetype.add {
