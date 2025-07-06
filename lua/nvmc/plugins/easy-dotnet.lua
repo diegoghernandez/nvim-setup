d21ffe77d36d5413503bb25bd78cd13747993fea
diegoghernandez <diegoJ4V@gmail.com>
2025-07-05 21:45:07 -0600

    Add filetype to easy-dotnet plugin

--- lua/nvmc/plugins/easy-dotnet.lua
+++ lua/nvmc/plugins/easy-dotnet.lua
@@ -1,6 +1,7 @@
 return {
   'GustavEikaas/easy-dotnet.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim', 'seblyng/roslyn.nvim' },
+  ft = 'cs',
   config = function()
     require('easy-dotnet').setup()
   end,
