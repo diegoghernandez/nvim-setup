d416972f2a8ed8d0184c49443b0e13947c0d7b54
diegoghernandez <diegoJ4V@gmail.com>
2025-09-24 11:52:21 -0600

    Increase priority of lsp in nvim-cmp

--- lua/nvmc/plugins/nvim-cmp.lua
+++ lua/nvmc/plugins/nvim-cmp.lua
@@ -115,7 +115,7 @@ return { -- Autocompletion
           -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
           group_index = 0,
         },
-        { name = 'nvim_lsp' },
+        { name = 'nvim_lsp', priority = 1000000 },
         { name = 'luasnip' },
         { name = 'path' },
         { name = 'nvim_lsp_signature_help' },
