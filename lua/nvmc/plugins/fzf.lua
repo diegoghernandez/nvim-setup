bccb20ca103dc5730028bf72050c5653c68341cd
diegoghernandez <diegoJ4V@gmail.com>
2025-10-22 15:10:22 -0600

    Chnage telescope for fzf-lua plugin

---
+++ lua/nvmc/plugins/fzf.lua
@@ -0,0 +1,107 @@
+return {
+  'ibhagwan/fzf-lua',
+  -- optional for icon support
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+      ---@module 'render-markdown'
+      ---@type render.md.UserConfig
+      opts = {},
+    },
+  },
+  -- or if using mini.icons/mini.nvim
+  -- dependencies = { "nvim-mini/mini.icons" },
+  opts = function(_, opts)
+    local config = require 'fzf-lua.config'
+    local actions = require 'fzf-lua.actions'
+
+    -- Quickfix
+    config.defaults.keymap.fzf['ctrl-q'] = 'select-all+accept'
+    config.defaults.keymap.fzf['ctrl-u'] = 'half-page-up'
+    config.defaults.keymap.fzf['ctrl-d'] = 'half-page-down'
+    config.defaults.keymap.fzf['ctrl-x'] = 'jump'
+    config.defaults.keymap.fzf['ctrl-f'] = 'preview-page-down'
+    config.defaults.keymap.fzf['ctrl-b'] = 'preview-page-up'
+    config.defaults.keymap.builtin['<c-f>'] = 'preview-page-down'
+    config.defaults.keymap.builtin['<c-b>'] = 'preview-page-up'
+
+    local img_previewer ---@type string[]?
+    for _, v in ipairs {
+      { cmd = 'ueberzug', args = {} },
+      { cmd = 'chafa', args = { '{file}', '--format=symbols' } },
+      { cmd = 'viu', args = { '-b' } },
+    } do
+      if vim.fn.executable(v.cmd) == 1 then
+        img_previewer = vim.list_extend({ v.cmd }, v.args)
+        break
+      end
+    end
+
+    return {
+      oldfiles = {
+        include_current_session = true,
+      },
+      previewers = {
+        builtin = {
+          extensions = {
+            ['png'] = img_previewer,
+            ['jpg'] = img_previewer,
+            ['jpeg'] = img_previewer,
+            ['gif'] = img_previewer,
+            ['webp'] = img_previewer,
+          },
+          ueberzug_scaler = 'fit_contain',
+          syntax_limit_b = 1024 * 100, -- 100KB
+        },
+      },
+      grep = {
+        rg_glob = true, -- enable glob parsing
+        glob_flag = '--iglob', -- case insensitive globs
+        glob_separator = '%s%-%-', -- query separator pattern (lua): ' --'
+      },
+    }
+  end,
+  keys = {
+    { '<c-j>', '<c-j>', ft = 'fzf', mode = 't', nowait = true },
+    { '<c-k>', '<c-k>', ft = 'fzf', mode = 't', nowait = true },
+    {
+      '<leader>,',
+      '<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>',
+      desc = 'Switch Buffer',
+    },
+    { '<leader>:', '<cmd>FzfLua command_history<cr>', desc = 'Command History' },
+    -- find
+    { '<leader>fb', '<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>', desc = 'Buffers' },
+    { '<leader>fg', '<cmd>FzfLua git_files<cr>', desc = 'Find Files (git-files)' },
+    { '<leader>fr', '<cmd>FzfLua oldfiles<cr>', desc = 'Recent' },
+    -- git
+    { '<leader>gc', '<cmd>FzfLua git_commits<CR>', desc = 'Commits' },
+    { '<leader>gs', '<cmd>FzfLua git_status<CR>', desc = 'Status' },
+    -- search
+    { '<leader>sf', '<cmd>FzfLua files<cr>', desc = 'Search files in current directory' },
+    { '<leader>s"', '<cmd>FzfLua registers<cr>', desc = 'Registers' },
+    { '<leader>sa', '<cmd>FzfLua autocmds<cr>', desc = 'Auto Commands' },
+    { '<leader>sb', '<cmd>FzfLua grep_curbuf<cr>', desc = 'Buffer' },
+    { '<leader>sc', '<cmd>FzfLua command_history<cr>', desc = 'Command History' },
+    { '<leader>sC', '<cmd>FzfLua commands<cr>', desc = 'Commands' },
+    { '<leader>sd', '<cmd>FzfLua diagnostics_document<cr>', desc = 'Document Diagnostics' },
+    { '<leader>sD', '<cmd>FzfLua diagnostics_workspace<cr>', desc = 'Workspace Diagnostics' },
+    { '<leader>sg', '<cmd>FzfLua grep<cr>', desc = 'Grep' },
+    { '<leader>sh', '<cmd>FzfLua help_tags<cr>', desc = 'Help Pages' },
+    { '<leader>sH', '<cmd>FzfLua highlights<cr>', desc = 'Search Highlight Groups' },
+    { '<leader>sj', '<cmd>FzfLua jumps<cr>', desc = 'Jumplist' },
+    { '<leader>sk', '<cmd>FzfLua keymaps<cr>', desc = 'Key Maps' },
+    { '<leader>sl', '<cmd>FzfLua loclist<cr>', desc = 'Location List' },
+    { '<leader>sM', '<cmd>FzfLua man_pages<cr>', desc = 'Man Pages' },
+    { '<leader>sm', '<cmd>FzfLua marks<cr>', desc = 'Jump to Mark' },
+    {
+      '<leader>sn',
+      '<cmd>FzfLua files cwd=~/.config/nvim<cr>',
+      desc = '[S]earch [N]eovim files',
+    },
+    { '<leader>sR', '<cmd>FzfLua resume<cr>', desc = 'Resume' },
+    { '<leader>sq', '<cmd>FzfLua quickfix<cr>', desc = 'Quickfix List' },
+  },
+}
--- lua/nvmc/plugins/nvim-lspconfig.lua
+++ lua/nvmc/plugins/nvim-lspconfig.lua
@@ -63,25 +63,30 @@ return {
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
+        map('grd', '<cmd>FzfLua lsp_definitions jump1 ignore_current_line=true<cr>', 'Goto Definition')
+        map('grr', '<cmd>FzfLua lsp_references jump1 ignore_current_line=true<cr>', 'References')
+        map('gri', '<cmd>FzfLua lsp_implementations jump1 ignore_current_line=true<cr>', 'Goto Implementation')
+        map('gry', '<cmd>FzfLua lsp_typedefs jump1 ignore_current_line=true<cr>', 'Goto T[y]pe Definition')
+
         -- Rename the variable under your cursor.
         --  Most Language Servers support renaming across files, etc.
-        map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+        -- map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
 
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
-        map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+        -- map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
 
         -- Find references for the word under your cursor.
-        map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        -- map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 
         -- Jump to the implementation of the word under your cursor.
         --  Useful when your language has ways of declaring types without an actual implementation.
-        map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        -- map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
         -- Jump to the definition of the word under your cursor.
         --  This is where a variable was first declared, or where a function is defined, etc.
         --  To jump back, press <C-t>.
-        map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+        -- map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header.
@@ -89,16 +94,16 @@ return {
 
         -- Fuzzy find all the symbols in your current document.
         --  Symbols are things like variables, functions, types, etc.
-        map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
+        -- map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
 
         -- Fuzzy find all the symbols in your current workspace.
         --  Similar to document symbols, except searches over your entire project.
-        map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+        -- map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
 
         -- Jump to the type of the word under your cursor.
         --  Useful when you're not sure what type a variable is and you want to see
         --  the definition of its *type*, not where it was *defined*.
-        map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
+        -- map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
         -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
         ---@param client vim.lsp.Client
@@ -239,6 +244,7 @@ return {
           },
         },
       },
+      tailwindcss = {},
     }
 
     -- Ensure the servers and tools above are installed
@@ -264,7 +270,7 @@ return {
       'prettier',
       'roslyn',
       'typos-lsp',
-      -- 'typescript-language-server',
+      'typescript-language-server',
     })
 
     require('mason').setup {
