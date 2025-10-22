b5dee2e2a12bbf4f386d45bb8506752530fa470c
diegoghernandez <diegoJ4V@gmail.com>
2025-10-22 15:08:39 -0600

    Add lazy propery to easy-dotnet plugin

--- lua/nvmc/plugins/easy-dotnet.lua
+++ lua/nvmc/plugins/easy-dotnet.lua
@@ -1,7 +1,8 @@
 return {
   'GustavEikaas/easy-dotnet.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim', 'seblyng/roslyn.nvim' },
+  lazy = true,
   ft = 'cs',
+  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim', 'seblyng/roslyn.nvim' },
   config = function()
     require('easy-dotnet').setup()
   end,
