9a0107dda773e7d3b1ec827cf47bde7f89999b6e
diegoghernandez <diegoJ4V@gmail.com>
2025-12-06 18:49:04 -0600

    Add if checks to activate some functionality depending of the plugin

--- lua/nvmc/plugins/nvim-lspconfig.lua
+++ lua/nvmc/plugins/nvim-lspconfig.lua
@@ -14,11 +14,13 @@ return {
 
     -- Allows extra capabilities provided by blink.cmp
     'saghen/blink.cmp',
-
     -- Allows extra capabilities provided by nvim-cmp
     'hrsh7th/nvim-cmp',
 
     'nvim-java/nvim-java',
+
+    'ibhagwan/fzf-lua',
+    'nvim-telescope/telescope.nvim',
   },
   config = function()
     -- Brief aside: **What is LSP?**
@@ -63,47 +65,49 @@ return {
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
-        map('grd', '<cmd>FzfLua lsp_definitions jump1 ignore_current_line=true<cr>', 'Goto Definition')
-        map('grr', '<cmd>FzfLua lsp_references jump1 ignore_current_line=true<cr>', 'References')
-        map('gri', '<cmd>FzfLua lsp_implementations jump1 ignore_current_line=true<cr>', 'Goto Implementation')
-        map('gry', '<cmd>FzfLua lsp_typedefs jump1 ignore_current_line=true<cr>', 'Goto T[y]pe Definition')
+        if package.loaded['fzf-lua'] then
+          map('grd', '<cmd>FzfLua lsp_definitions jump1 ignore_current_line=true<cr>', 'Goto Definition')
+          map('grr', '<cmd>FzfLua lsp_references jump1 ignore_current_line=true<cr>', 'References')
+          map('gri', '<cmd>FzfLua lsp_implementations jump1 ignore_current_line=true<cr>', 'Goto Implementation')
+          map('gry', '<cmd>FzfLua lsp_typedefs jump1 ignore_current_line=true<cr>', 'Goto T[y]pe Definition')
+        elseif package.loaded['telescope'] then
+          -- Find references for the word under your cursor.
+          map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+          -- Jump to the implementation of the word under your cursor.
+          --  Useful when your language has ways of declaring types without an actual implementation.
+          map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+          -- Jump to the definition of the word under your cursor.
+          --  This is where a variable was first declared, or where a function is defined, etc.
+          --  To jump back, press <C-t>.
+          map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+          -- Fuzzy find all the symbols in your current document.
+          --  Symbols are things like variables, functions, types, etc.
+          map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
+
+          -- Fuzzy find all the symbols in your current workspace.
+          --  Similar to document symbols, except searches over your entire project.
+          map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+
+          -- Jump to the type of the word under your cursor.
+          --  Useful when you're not sure what type a variable is and you want to see
+          --  the definition of its *type*, not where it was *defined*.
+          map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
+        end
 
         -- Rename the variable under your cursor.
         --  Most Language Servers support renaming across files, etc.
-        -- map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+        map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
 
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
-        -- map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
-
-        -- Find references for the word under your cursor.
-        -- map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-        -- Jump to the implementation of the word under your cursor.
-        --  Useful when your language has ways of declaring types without an actual implementation.
-        -- map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-        -- Jump to the definition of the word under your cursor.
-        --  This is where a variable was first declared, or where a function is defined, etc.
-        --  To jump back, press <C-t>.
-        -- map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+        map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
 
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header.
         map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-        -- Fuzzy find all the symbols in your current document.
-        --  Symbols are things like variables, functions, types, etc.
-        -- map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
-
-        -- Fuzzy find all the symbols in your current workspace.
-        --  Similar to document symbols, except searches over your entire project.
-        -- map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
-
-        -- Jump to the type of the word under your cursor.
-        --  Useful when you're not sure what type a variable is and you want to see
-        --  the definition of its *type*, not where it was *defined*.
-        -- map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
         -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
         ---@param client vim.lsp.Client
@@ -193,12 +197,14 @@ return {
     --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
 
-    -- ## Blink config
-    -- local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-    -- ## Nvim-cmp config
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+    if package.loaded['blink'] then
+      -- ## Blink config
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+    elseif package.loaded['cmp_nvim_lsp'] then
+      -- ## Nvim-cmp config
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+    end
 
     -- Enable the following language servers
     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -270,7 +276,7 @@ return {
       'prettier',
       'roslyn',
       'typos-lsp',
-      'typescript-language-server',
+      -- 'tsgo',
     })
 
     require('mason').setup {
