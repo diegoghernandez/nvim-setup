8d6d1d616c739c297f51459f834bc3d09005edb7
diegoghernandez <diegoJ4V@gmail.com>
2026-02-09 19:41:36 -0600

    Disabling typescript-tools plugin

--- lua/nvmc/plugins/typescript-tools.lua
+++ lua/nvmc/plugins/typescript-tools.lua
@@ -1,14 +1,25 @@
 return {
   'pmizio/typescript-tools.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  enabled = false,
   opts = {},
   config = function()
     require('typescript-tools').setup {
+      filetypes = {
+        'javascript',
+        'javascriptreact',
+        'typescript',
+        'typescriptreact',
+        'vue',
+      },
       on_attach = function(client)
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
       end,
       settings = {
+        tsserver_plugins = {
+          '@vue/typescript-plugin',
+        },
         jsx_close_tag = {
           enable = true,
           filetypes = { 'javascriptreact', 'typescriptreact' },
