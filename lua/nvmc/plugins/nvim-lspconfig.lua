0204e399ae30517e2513d09ef541d23e2c7f151a
diegoghernandez <diegoJ4V@gmail.com>
2026-02-09 19:39:54 -0600

    Change nvim-lspconfig
    - Change vim lsp protocol methods to the latest names 'textDocument'
    
    - Comment blink capabilities for nvim-cmp
    
    - Comment Eslint autocommand
    
    - Add Harper lsp
    
    - Add typescript with vue plugin and tsgo LSPs
    
    - Implement new neovim lsp config
    
    - Add jsonls and yamlls lsp
    
    - Remove mason-lspconfig

--- lua/nvmc/plugins/nvim-lspconfig.lua
+++ lua/nvmc/plugins/nvim-lspconfig.lua
@@ -111,7 +111,7 @@ return {
 
         -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
         ---@param client vim.lsp.Client
-        ---@param method vim.lsp.protocol.Method
+        ---@param method vim.lsp.protocol.Method.ClientToServer
         ---@param bufnr? integer some lsp support methods only in specific files
         ---@return boolean
         local function client_supports_method(client, method, bufnr)
@@ -128,7 +128,7 @@ return {
         --
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+        if client and client_supports_method(client, 'textDocument/documentHighlight', event.buf) then
           local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
@@ -155,7 +155,7 @@ return {
         -- code, if the language server you are using supports them
         --
         -- This may be unwanted, since they displace some of your code
-        if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+        if client and client_supports_method(client, 'textDocument/inlayHint', event.buf) then
           map('<leader>th', function()
             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
           end, '[T]oggle Inlay [H]ints')
@@ -197,14 +197,12 @@ return {
     --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
 
-    if package.loaded['blink'] then
-      -- ## Blink config
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-    elseif package.loaded['cmp_nvim_lsp'] then
-      -- ## Nvim-cmp config
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-    end
+    -- ## Blink config
+    -- local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+    -- ## Nvim-cmp config
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
     -- Enable the following language servers
     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -230,15 +228,44 @@ return {
           packageManager = 'pnpm',
         },
         ---@diagnostic disable-next-line: unused-local
-        on_attach = function(client, bufnr)
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            buffer = bufnr,
-            command = 'EslintFixAll',
-          })
-        end,
+        -- on_attach = function(client, bufnr)
+        --   vim.api.nvim_create_autocmd('BufWritePre', {
+        --     buffer = bufnr,
+        --     command = 'EslintFixAll',
+        --   })
+        -- end,
       },
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+      harper_ls = {
+        filetypes = {
+          'asciidoc',
+          'c',
+          'cpp',
+          'cs',
+          'gitcommit',
+          'go',
+          'html',
+          'java',
+          'javascript',
+          'lua',
+          'markdown',
+          'nix',
+          'python',
+          'ruby',
+          'rust',
+          'swift',
+          'toml',
+          'typescript',
+          'typescriptreact',
+          'haskell',
+          'cmake',
+          'typst',
+          'php',
+          'dart',
+          'clojure',
+          'sh',
+          'yaml',
+        },
+      },
       lua_ls = {
         settings = {
           Lua = {
@@ -251,7 +278,34 @@ return {
         },
       },
       tailwindcss = {},
+      ts_ls = {
+        filetypes = {
+          'vue',
+        },
+        init_options = {
+          plugins = {
+            {
+              name = '@vue/typescript-plugin',
+              location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+              languages = { 'vue' },
+              configNamespace = 'typescript',
+            },
+          },
+        },
+      },
+      tsgo = {},
+      vue_ls = {},
     }
+
+    for name, config in pairs(servers) do
+      local config = config or {}
+      -- This handles overriding only values explicitly passed
+      -- by the server configuration above. Useful when disabling
+      -- certain features of an LSP (for example, turning off formatting for ts_ls)
+      config.capabilities = vim.tbl_deep_extend('force', {}, capabilities, config.capabilities or {})
+      vim.lsp.config(name, config)
+      vim.lsp.enable(name)
+    end
 
     -- Ensure the servers and tools above are installed
     --
@@ -267,16 +321,17 @@ return {
     -- You can add other tools here that you want Mason to install
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
+
     vim.list_extend(ensure_installed, {
       'csharpier',
       'google-java-format',
-      'jdtls',
+      -- 'jdtls',
       'stylua', -- Used to format Lua code
       'prettierd',
       'prettier',
       'roslyn',
-      'typos-lsp',
-      'ts_ls',
+      'jsonls',
+      'yamlls',
     })
 
     require('mason').setup {
@@ -287,37 +342,5 @@ return {
     }
 
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-    require('mason-lspconfig').setup {
-      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-      automatic_installation = false,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for ts_ls)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          -- vim.lsp.enable(server_name)
-          vim.lsp.config(server_name, server)
-          -- require('lspconfig')[server_name].setup(server)
-        end,
-        jdtls = function()
-          require('java').setup {
-            jdk = {
-              auto_install = false,
-            },
-          }
-
-          vim.lsp.config('jdtls', {})
-          -- require('lspconfig').jdtls.setup {
-          --   -- Your custom nvim-java configuration goes here
-          -- }
-        end,
-        ['ts_ls'] = function()
-          -- do nothing, managed by typescript-tools
-        end,
-      },
-    }
   end,
 }
