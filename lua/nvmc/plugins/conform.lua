84e13abb7d85a7472c4a489b54a1be54d055054c
diegoghernandez <diegoJ4V@gmail.com>
2026-02-09 19:25:08 -0600

    Change vue conform config

--- lua/nvmc/plugins/conform.lua
+++ lua/nvmc/plugins/conform.lua
@@ -41,7 +41,7 @@ return { -- Autoformat
       javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
       typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
       htmlangular = { 'prettierd', 'prettier', stop_after_first = true },
-      vue = { 'prettierd', 'prettier', stop_after_first = true },
+      vue = { 'prettierd', 'prettier', lsp_format = 'never', stop_after_first = true },
       css = { 'prettierd', 'prettier', stop_after_first = true },
       scss = { 'prettierd', 'prettier', stop_after_first = true },
       html = { 'prettierd', 'prettier', stop_after_first = true },
